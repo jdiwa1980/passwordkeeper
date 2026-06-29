7c83c12ae31686d2a8167f774f2400098fb83b83
jdiwa1980 <35224714+jdiwa1980@users.noreply.github.com>
2026-06-30 02:28:36 +0800

    Version 1.0 6/30/26

--- backend/models/Record.js
+++ backend/models/Record.js
@@ -2,11 +2,6 @@ const mongoose = require("mongoose");
 
 const recordSchema = new mongoose.Schema(
     {
-        owner: {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: "User",
-            required: true
-        },
         account: {
             type: String,
             required: true,
@@ -20,6 +15,12 @@ const recordSchema = new mongoose.Schema(
         description: {
             type: String,
             required: true,
+        },
+        
+        owner: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+            required: true
         },
     },
     {
