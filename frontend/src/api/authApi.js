df59748718b23276002ea3becc8ccf389a7ea21b
jdiwa1980 <35224714+jdiwa1980@users.noreply.github.com>
2026-07-24 02:43:49 +0800

    updated authAPI.js url

--- frontend/src/api/authApi.js
+++ frontend/src/api/authApi.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:5000/api/auth";
+const API_URL = import.meta.env.VITE_BACKEND_URL;
 
 export const login = (data) =>
     axios.post(`${API_URL}/login`, data);
